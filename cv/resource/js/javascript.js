f66c211e125772ee22a6c1a71cad00966b372aed
belvoixnicolas <belvoixnicolas1997@gmail.com>
2018-05-17 01:25:55 +0200

    bfd

--- cv/resource/js/javascript.js
+++ cv/resource/js/javascript.js
@@ -7,7 +7,7 @@ var prenom = questionPrenom.toLowerCase();
 if (nom == 'belvoix' && prenom == 'nicolas') {
   var ident = ['Belvoix', 'Nicolas', '57 Rue Voltaire', 'Charleville-Mézières (08000)', '07.82.02.13.27', 'belvoixnicolas1997@gmail.com'];
   document.getElementById('nom_prenom').innerHTML = ident[0] + ' ' + ident[1];
-  document.getElementById('lieu').innerHTML = ident[2] + '<br/>' + ident[3];
+  document.getElementById('lieu').innerHTML = ident[2] + ' ' + ident[3];
   document.getElementById('tel').innerHTML = ident[4];
   document.getElementById('mail').innerHTML = ident[5];
 }
