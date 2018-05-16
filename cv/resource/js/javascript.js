9761b77a76a4b7e890b051314ba755baaaf08d57
belvoixnicolas <belvoixnicolas1997@gmail.com>
2018-05-17 00:05:20 +0200

    t

--- cv/resource/js/javascript.js
+++ cv/resource/js/javascript.js
@@ -1,12 +1,11 @@
-var questionNom = prompt('Votre nom.');
-var questionPrenom = prompt('Votre prénom.');
+var questionNom = 'Belvoix'/*prompt('Votre nom.')*/;
+var questionPrenom = 'Nicolas'/*prompt('Votre prénom.')*/;
 
 var nom = questionNom.toLowerCase();
 var prenom = questionPrenom.toLowerCase();
 
 if (nom == 'belvoix' && prenom == 'nicolas') {
   var ident = ['Belvoix', 'Nicolas', '57 Rue Voltaire', 'Charleville-Mézières (08000)', '07.82.02.13.27', 'belvoixnicolas1997@gmail.com'];
-  alert(ident[0] + ' ' + ident[1]);
   document.getElementById('nom_prenom').innerHTML = ident[0] + ' ' + ident[1];
   document.getElementById('lieu').innerHTML = ident[2] + '<br/>' + ident[3];
   document.getElementById('tel').innerHTML = ident[4];
