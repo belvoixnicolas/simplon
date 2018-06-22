f59d77cd40b6c00c902f85f7be5f5c8839161752
belvoixnicolas <belvoixnicolas1997@gmail.com>
2018-06-22 11:43:43 +0200

    k

--- php/traitement.php
+++ php/traitement.php
@@ -13,6 +13,21 @@
           echo '<p>Le mot de passe est ' . htmlspecialchars($_POST['psw']) . '.</p>';
           echo '<p>Votre adresse ip est ' . $_SERVER['REMOTE_ADDR'] . '.</p>';
           echo '<p>L\'url de cette page est ' . $_SERVER['PHP_SELF'] . '.</p>';
+
+          $mail = $_POST['mail'];
+          $aparue = 0;
+          $text = '';
+          for ($i=0; $i < strlen($mail); $i++) {
+            if ($aparue == 1) {
+              $text = $text . $mail[$i];
+            }
+
+            if ($mail[$i] == '@') {
+              $aparue = 1;
+            }
+          }
+
+          echo $text;
         }
       ?>
     </body>
